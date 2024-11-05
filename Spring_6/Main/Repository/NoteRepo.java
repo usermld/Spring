e47bb386319cc1dda2f789d3d7c55c20f5eb3cb4
MU <m.iouriev@gmail.com>
2024-11-05 13:36:34 +0700

    spring 6

---
+++ Spring_6/Main/Repository/NoteRepo.java
@@ -0,0 +1,16 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.springhw6.model.Note;
+
+import java.util.Optional;
+
+@Repository
+public interface NoteRepo extends JpaRepository<Note, Long> {
+
+    /**
+     * Найти заметку по id
+     * @param id must not be {@literal null}.
+     * @return возвращает заметку.
+     */
+    Optional<Note> findById(Long id);
+}
---
+++ Spring_6/Main/Service/NoteService.java
@@ -0,0 +1,15 @@
+import ru.geekbrains.springhw6.model.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    Note createNote(Note note);
+
+    List<Note> getAllNotes();
+
+    Note getNoteById(Long id);
+
+    Note updateNote(Note note);
+
+    void deleteNote(Long id);
+}
